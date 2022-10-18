d82139249f596707baa911dcb5e76f9b053876e3
Demarcus Sales <50031416+dthecsguy@users.noreply.github.com>
2022-10-18 05:01:02 -0700

    Create lab3_2.sql

---
+++ lab3/lab3_2.sql
@@ -0,0 +1,23 @@
+drop table if exists Home cascade;
+drop table if exists Place cascade;
+drop table if exists Telephone cascade;
+
+create table Place(
+  address text not null,
+  primary key (address)
+);
+
+create table Telephone(
+  phone_no char(11) not null,
+  address text not null,
+  foreign key (address) references Place(address),
+  primary key (phone_no, address)
+);
+
+create table Home(
+  address text not null,
+  phone_no char(11) not null,
+  primary key (address, phone_no),
+  foreign key (address) references Place(address),
+  foreign key (phone_no) references Telephone(phone_no)
+);
