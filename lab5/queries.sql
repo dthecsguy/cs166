f700a86399bece08e8c3828216e8b585ac83969c
Demarcus Sales <50031416+dthecsguy@users.noreply.github.com>
2022-10-26 16:09:14 -0700

    Update queries.sql

--- lab5/queries.sql
+++ lab5/queries.sql
@@ -1,1 +1,23 @@
+select distinct pid
+from parts
+where cost < 10;
+
+select pname
+from parts
+inner join catalog
+on parts.pid = catalog.pid and parts.cost < 10;
+
+select address
+from suppliers
+inner join catalog
+on suppliers.sid = catalog.sid and suppliers.sname = "Fire Hydrant Cap";
+
+select sname
+from suppliers
+inner join catalog
+on suppliers.sid = catalog.sid and parts.color = "green";
+
+select suppliers.sname, parts.pname
+from suppliers
+inner join catalog;
 
